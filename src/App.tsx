fb17ee840b381a38659c3d787596129930d828fd
Nghia Mai <nghia.mai@foodpanda.com>
2022-06-22 05:53:12 -0400

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 
 import Favourites from "./Favourites/Favourites";
+import FoodDescriptionPage from "./FoodDescriptionPage/FoodDescriptionPage";
 import FoodSwipe from "./FoodSwipe/FoodSwipe";
 import Homescreen from "./Homescreen/Homescreen";
 import RestaurantListingPage from "./RestaurantListingPage/RestaurantListingPage";
 import SearchPage from "./SearchPage/SearchPage";
 import YumlistPage from "./YumlistPage/YumlistPage";
-import FoodDescriptionPage from "./FoodDescriptionPage/FoodDescriptionPage";
 
 if (process.env.NODE_ENV === "development") {
   require("mocks");
@@ -23,7 +23,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Homescreen />} />
-          <Route path="/swipe" element={<FoodSwipe />} />
+          <Route path="/foodswipe" element={<FoodSwipe />} />
           <Route path="search" element={<SearchPage />} />
           <Route path="restaurant" element={<RestaurantListingPage />} />
           <Route path="favourites" element={<Favourites />} />
