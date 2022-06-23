1c3020ce2171f3f5bab2b573f13a472b2fd75535
Yi Long (Darren) <yilong.tan@foodpanda.com>
2022-06-23 15:41:26 +0800

    Add more images

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,12 @@ import image2 from "assets/2.jpg";
 import image3 from "assets/3.jpg";
 import image4 from "assets/4.jpg";
 import image5 from "assets/5.jpg";
+import image6 from "assets/6.jpg";
+import image7 from "assets/7.jpg";
+import image8 from "assets/8.jpg";
+import image9 from "assets/9.jpg";
+import image10 from "assets/10.jpg";
+import image11 from "assets/11.jpg";
 
 export const mapDishIdToImage: Record<string, any> = {
   "1": image1,
@@ -10,4 +16,10 @@ export const mapDishIdToImage: Record<string, any> = {
   "3": image3,
   "4": image4,
   "5": image5,
+  "6": image6,
+  "7": image7,
+  "8": image8,
+  "9": image9,
+  "10": image10,
+  "11": image11,
 };
